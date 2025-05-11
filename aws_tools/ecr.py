691162523e6ac610582f3a25f29ab2c0c0df56df
Benoit Favier <benoitfamillefavier@gmail.com>
2025-05-11 13:13:19 +0200

    improved ecr resources

--- aws_tools/ecr.py
+++ aws_tools/ecr.py
@@ -3,20 +3,28 @@ import boto3
 ecr = boto3.client('ecr')
 
 
-def list_images(repository_name: str):
+def list_repositories() -> list[str]:
+    """
+    Return all existing repository names
+    """
+    paginator = ecr.get_paginator('describe_repositories')
+    return [repo["repositoryName"] for page in paginator.paginate() for repo in page['repositories']]
+
+
+def list_image_tags(repository_name: str) -> list[str]:
     """
     Return all the tagged ecr images from the ecr repository
     """
-    all_images = []
+    all_image_tags = []
     next_token = None
     while True:
         response = ecr.list_images(
             repositoryName=repository_name,
             filter={'tagStatus': 'TAGGED'},
-            nextToken=next_token
+            **(dict(nextToken=next_token) if next_token is not None else dict())
         )
-        images, next_token = response["imageIds"], response["nextToken"]
-        all_images.append(images)
+        images, next_token = response["imageIds"], response.get("nextToken", None)
+        all_image_tags.extend([img["imageTag"]for img in images])
         if next_token is None:
             break
-    return all_images
+    return all_image_tags
