1ef25533a88010a0227f60a3634c2059c63a1f02
BenoitFavier <bade@webmails.top>
2026-02-13 23:14:18 -0600

    draft of agent

--- aws_tools/bedrock.py
+++ aws_tools/bedrock.py
@@ -1,6 +1,7 @@
 import aioboto3
 from base64 import b64decode, b64encode
-from typing import Literal, Optional, Any, Annotated
+from collections import defaultdict
+from typing import Literal, Optional, Any, Annotated, Type, TypeVar
 from pydantic import BaseModel, Field, BeforeValidator, SerializerFunctionWrapHandler, SerializationInfo
 from pydantic.functional_serializers import WrapSerializer
 
@@ -111,6 +112,31 @@ class BedrockContentBlock(BaseModel):
     toolResult: Optional[BedrockToolResult] = None
 
 
+class BedrockSystemContentBlock(BaseModel):
+    """
+    https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_SystemContentBlock.html
+    """
+
+    class BedrockCachePointBlock(BaseModel):
+        """
+        https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_CachePointBlock.html
+        """
+        type: Literal["default"]
+        ttl: Literal["5m", "1h"] | None = None
+    
+    class BedrockGuardrailConverseContentBlock(BaseModel):
+        """
+        https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_GuardrailConverseContentBlock.html
+        TODO: I did not dig deeper yet in the structure
+        """
+        image: Any
+        text: Any
+
+    cachePoint: BedrockCachePointBlock | None = None
+    guardContent: BedrockGuardrailConverseContentBlock | None = None
+    system: str | None = None
+
+
 class BedrockMessage(BaseModel):
     """
     https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Message.html
@@ -156,6 +182,7 @@ class BedrockConverseRequest(BaseModel):
     modelId: str
     messages: list[BedrockMessage]
     inferenceConfig: Optional[BedrockInferenceConfig] = None
+    system: BedrockSystemContentBlock
     toolConfig: Optional[BedrockToolConfig] = None
 
     @property
@@ -281,3 +308,118 @@ class Bedrock:
 
     async def converse_async(self, payload: BedrockConverseRequest) -> BedrockConverseResponse:
         return BedrockConverseResponse(**await self._client.converse(**payload.model_dump(mode="json", exclude_none=True)))
+
+
+class AgentTool(BaseModel):
+    """
+    Derive this base class to implement a tool that LLMs can call.
+    The description of the tool must be the class docstring.
+    The arguments to be provided by the LLM must be fields of the class, the description of which is described with pydantic's 'argument = Field(..., description="the tool argument description")'.
+    """
+
+    async def __call__(self, **tool_secrets) -> Any:
+        """
+        Any documentation here is invisible to the LLM.
+        The implementation of the actual tool is in the class' __call__ method.
+        In addition to the tool args that are fields of this object,
+        any additional kwarg passed to the __call__ method can be used for the logic while staying hidden from the LLM
+        """
+        raise NotImplementedError()
+    
+    @classmethod
+    def definition(cls) -> BedrockToolConfig.BedrockTool:
+        """
+        """
+        return BedrockToolConfig.BedrockTool(
+            toolSpec=BedrockToolConfig.BedrockTool.BedrockToolSpec(
+                inputSchema=cls.model_json_schema()
+            ),
+            name=cls.__name__,
+            description="\n".join(s.strip() for s in cls.__doc__.split("\n") if len(s.strip()) > 0) if cls.__doc__ is not None else None
+        )
+
+
+T = TypeVar("T", bound=Type[AgentTool])
+
+
+class Agent:
+    """
+    Derive this base class agent is an LLM with tools registered
+    """
+    tools: dict[str, Type[AgentTool]] = {}
+    tool_config: BedrockToolConfig | None = None
+
+    def __init_subclass__(cls, **kwargs):
+        """
+        Each derived class have a distinct set of 'tools' and 'tool_config' objects
+        """
+        super().__init_subclass__(**kwargs)
+        cls.tools = {}
+        cls.tool_config = None
+
+    def __init__(self, bedrock_client: Bedrock, model_id: str, system_prompt: str | None):
+        self.bedrock_client = bedrock_client
+        self.model_id = model_id
+        self.system_prompt = system_prompt
+
+    @classmethod
+    def register_tool(cls, new_tool: T) -> T:
+        """
+        A decorator to apply on top of new tool to register into an agent class
+        """
+        cls.tools[new_tool.__name__] = new_tool
+        cls.bedrock_tool_config = BedrockToolConfig(
+            tools=[T.definition() for T in cls.tools.values()]
+        )
+        return new_tool
+
+    async def converse_async(self, conversation_id: str, history: list[BedrockMessage], user_message: BedrockContentBlock, inference_config: BedrockInferenceConfig | None = None, tool_secrets: dict[str, dict]=defaultdict(dict)) -> BedrockConverseResponse:
+        """
+        Returns a response from the LLM.
+        """
+        history.append(BedrockMessage(role="user"))
+        while True:
+            message = await self._agent_turn_async(inference_config)
+            history.append(message)
+            tool_uses = [block.toolUse for block in message.content if block.toolUse is not None]
+            if len(tool_uses) == 0:
+                break
+            else:
+                # TODO: logic for budget
+                ...
+
+    async def _agent_turn_async(self, inference_config: BedrockInferenceConfig | None) -> BedrockMessage:
+        """
+        """
+        payload = BedrockConverseRequest(
+            modelId=self.model_id,
+            inferenceConfig=inference_config,
+            system=BedrockSystemContentBlock(system=self.system_prompt),
+            toolConfig=self.tool_config
+        )
+        response = await self.bedrock_client.converse_async(payload)
+        return response.output.message
+
+    async def _call_tool(self, tool_use: BedrockContentBlock.BedrockToolUse, tool_secrets: dict[str, dict]) -> BedrockContentBlock.BedrockToolResult:
+        """
+        """
+        try:
+            Tool = self.tools[tool_use.name]
+            tool = Tool(**tool_use.input)
+            result = await tool(**tool_secrets[tool_use.name])
+        except Exception as e:
+            return BedrockContentBlock.BedrockToolResult(
+                content=[
+                    BedrockContentBlock.BedrockToolResult.BedrockToolResultContent(json={})
+                ],
+                toolUseId=tool_use.toolUseId,
+                status="error"
+            )
+        else:
+            return BedrockContentBlock.BedrockToolResult(
+                content=[
+                    BedrockContentBlock.BedrockToolResult.BedrockToolResultContent(json={"result": result})
+                ],
+                toolUseId=tool_use.toolUseId,
+                status="success"
+            )
